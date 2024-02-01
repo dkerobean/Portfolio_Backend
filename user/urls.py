045ab3b369188a2985cc5c2f607d3ac8498e353a
Dickson <dkerobean@gmail.com>
2024-02-01 10:34:17 +0000

    done with profile views and serializer

---
+++ user/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'profiles', views.ProfileViewSet, basename='profile')
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
