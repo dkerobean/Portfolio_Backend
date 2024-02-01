67d3bf0661064e95756b247466540d042fcc8608
Dickson <dkerobean@gmail.com>
2024-02-01 11:02:50 +0000

    done with contact

--- user/views.py
+++ user/views.py
@@ -1,8 +1,13 @@
 from rest_framework import viewsets
-from .serializers import ProfileSerializer
-from .models import Profile
+from .serializers import ProfileSerializer, ContactSerializer
+from .models import Profile, Contact
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+
+class ContactViewSet(viewsets.ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
